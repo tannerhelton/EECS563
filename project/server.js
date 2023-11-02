dc736947c950db81c41693ada696ab0011a061a6
Tanner Helton <tanner@tannerhelton.com>
2023-11-02 12:08:30 -0500

    lots of progress

--- project/server.js
+++ project/server.js
@@ -5,16 +5,23 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static("public"));
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Define User Schema for MongoDB
 const userSchema = new mongoose.Schema({
@@ -41,20 +48,28 @@ io.use(authenticateToken);
 // Socket connection
 io.on("connection", (socket) => {
   console.log("A user connected");
-
-  // Emit online users
-  const onlineUsers = Array.from(io.sockets.sockets.values()).map(
-    (s) => s.user.username
-  );
-  io.emit("online-users", onlineUsers);
+  updateOnlineUsers();
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
-    io.emit(
-      "online-users",
-      onlineUsers.filter((username) => username !== socket.user.username)
-    );
+    updateOnlineUsers();
   });
+
+  socket.on("join-chat", (username) => {
+    socket.user.username = username;
+    updateOnlineUsers();
+  });
+
+  socket.on("send-message", (data) => {
+    io.emit("receive-message", data);
+  });
+
+  function updateOnlineUsers() {
+    const onlineUsers = Array.from(io.sockets.sockets.values()).map(
+      (s) => s.user.username
+    );
+    io.emit("online-users", onlineUsers);
+  }
 });
 
 app.post("/register", async (req, res) => {
@@ -65,25 +80,42 @@ app.post("/register", async (req, res) => {
       password: hashedPassword,
     });
     await user.save();
-    res.status(201).send("User created");
-  } catch {
-    res.status(500).send();
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (user == null) return res.status(400).send("Cannot find user");
-
-  if (await bcrypt.compare(req.body.password, user.password)) {
     const accessToken = jwt.sign(
       { username: user.username },
       process.env.JWT_SECRET
     );
-    res.json({ accessToken });
-  } else {
-    res.send("Not Allowed");
+    res.cookie("token", accessToken, { httpOnly: true });
+    res.status(201).json({ accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
   }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (user == null) return res.status(400).send("Cannot find user");
+
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const accessToken = jwt.sign(
+        { username: user.username },
+        process.env.JWT_SECRET
+      );
+      res.cookie("token", accessToken, { httpOnly: true });
+      res.json({ accessToken });
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.send("Logged out");
 });
 
 server.listen(3000, () => {
